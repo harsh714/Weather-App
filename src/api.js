aefbe1367da488061bd5066344b50353241a13c1
Harsh Tiwari <harshtiwari7588@gmail.com>
2023-10-04 15:33:51 +0530

    Last Commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f360c07448msh9eca5e02a4ba6dfp1271cfjsn1bfc491a4421',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "360ac58cfd6e9fa6408da05b9c289ad9";
